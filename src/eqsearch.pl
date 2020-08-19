15c7640798f1db50d81d34d1c754f4c32e48baa2
王亮 <wangliang0222@gmail.com>
2020-08-20 00:56:02 +0800

    优化流程

--- src/eqsearch.pl
+++ src/eqsearch.pl
@@ -10,7 +10,7 @@ use lib $FindBin::Bin;
 $ENV{SAC_DISPLAY_COPYRIGHT}=0;
 
 my ($tempdir, $workdir) = @ARGV;
-my $threshold = 10;
+my $threshold = 15;
 match ($tempdir, $workdir);
 
 sub match {
@@ -18,38 +18,36 @@ sub match {
     my ($id) = (split m/\//, $tempdir)[-1];
     my $num = 0;
     my $corfile = " ";
-    my ($b, $e, $evlo, $evla, $evdp, $kztime, $kzdate);
-    foreach (glob "$tempdir/*.BH?") {
+    my ($evlo, $evla, $evdp, $kztime, $kzdate);
+    unlink glob "$workdir/*.cor";
+    foreach (glob "$tempdir/*.[enz]") {
         my ($file) = (split m/\//, $_)[-1];
         next unless (-e "$workdir/$file");
         (undef, $evlo, $evla, $evdp) = split m/\s+/, `saclst evlo evla evdp f $_`;
         (undef, $kzdate, $kztime) = split m/\s+/, `saclst kzdate kztime f $workdir/$file`;
-        my (undef, $bi, $ei) = split m/\s+/, `eqcor $workdir/$file $_ $workdir/${id}-${file}.cor`;
-        $b = $bi unless (defined($b));
-        $e = $ei unless (defined($e));
-        $b = min($b, $bi);
-        $e = max($e, $ei);
+        system "eqcor $workdir/$file $_ $workdir/${id}-${file}.cor";
         $num++;
         $corfile = "$corfile $workdir/${id}-${file}.cor";
+        open(SAC, "| sac") or die "Error in opening sac\n";
+        print SAC "wild echo off \n";
+        print SAC "cuterr fillz\n";
+        print SAC "cut 0 86400\n";
+        print SAC "r $workdir/${id}-${file}.cor\n";
+        print SAC "w over\n";
+        print SAC "q\n";
+        close(SAC);
     }
-    sum($id, $kzdate, $kztime, $evlo, $evla, $evdp, $b, $e, $num, $corfile) if ($num > 0);
+    sum("$workdir/result_${id}.txt", $kzdate, $kztime, $evlo, $evla, $evdp, $num, $corfile) if ($num > 0);
 }
 sub sum {
-    my ($id, $kzdate, $kztime, $evlo, $evla, $evdp, $b, $e, $num, $corfile) = @_;
-    open(SAC, "| sac") or die "Error in opening sac\n";
-    print SAC "wild echo off \n";
-    print SAC "cuterr fillz\n";
-    print SAC "cut $b $e\n";
-    print SAC "r $corfile\n";
-    print SAC "write over\n";
-    print SAC "q\n";
-    close(SAC);
+    my ($result, $kzdate, $kztime, $evlo, $evla, $evdp, $num, $corfile) = @_;
     my @info = split m/\n/, `eqsum $threshold $num $corfile`;
-    open (OUT, "> result_$id.txt") or die;
+    open (OUT, "> $result") or die;
     foreach (@info) {
         my ($time, $cc, $mad, $th) = (split m/\s+/)[1..4];
         print OUT "$kzdate $kztime $time $evlo $evla $evdp $mad $th $cc\n";
-        print "$kzdate $kztime $time $evlo $evla $evdp $mad $th $cc\n";
     }
     close (OUT);
+    my @to_delete = split m/\s+/, $corfile;
+    unlink @to_delete;
 }
