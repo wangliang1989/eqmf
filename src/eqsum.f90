362f453651a9e138a103974929ede0f1b8a824c9
王亮 <wangliang0222@gmail.com>
2020-08-20 21:16:23 +0800

    fix bug

--- src/eqsum.f90
+++ src/eqsum.f90
@@ -4,7 +4,7 @@ implicit none
 character(len=1024) :: arg
 character(len=:) ,allocatable :: file
 integer :: i, j, num, flag
-real, allocatable, dimension(:) :: data, x
+real, allocatable, dimension(:) :: data, x, nums
 real :: threshold, time, sigma, th, mad, mean
 type(sachead) :: head
 
@@ -21,10 +21,19 @@ if (flag /= 0) then
     write(0,*)"can not open file ", file
 end if
 
+allocate(nums(1 : head%npts))
+nums = num
 allocate(x(1 : head%npts))
 x = data
+do j = 1, head%npts
+    if (data(j) == 0) then
+        nums(j) = nums(j) - 1
+    end if
+    if (nums(j) == 0) then
+        nums(j) = 1
+    end if
+end do
 deallocate(data)
-
 i = 4
 do while (i <= command_argument_count())
     call get_command_argument(i, arg)
@@ -34,12 +43,20 @@ do while (i <= command_argument_count())
         write(0,*)"can not open file", file
     end if
     x = x + data
+    do j = 1, head%npts
+        if (data(j) == 0) then
+            nums(j) = nums(j) - 1
+        end if
+        if (nums(j) == 0) then
+            nums(j) = 1
+        end if
+    end do
     deallocate(data)
     deallocate(file)
     i = i + 1
 end do
 
-x = x / num
+x = x / nums
 mean = sum(x) / head%npts
 sigma = sqrt (sum((x - mean) * (x - mean)) / head%npts)
 mad = sigma / 1.4826
